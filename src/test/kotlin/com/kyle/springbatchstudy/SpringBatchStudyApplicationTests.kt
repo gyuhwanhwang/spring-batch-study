23a151882e78a4536ff1997c3243388839f434b8
gyuhwanhwang <ghh@kakao.com>
2023-10-15 00:21:54 +0900

    Init: project init

---
+++ src/test/kotlin/com/kyle/springbatchstudy/SpringBatchStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kyle.springbatchstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

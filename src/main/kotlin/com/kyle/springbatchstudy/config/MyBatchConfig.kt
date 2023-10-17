d15b10c073edd80253291c4dca03cda09b305cd1
gyuhwanhwang <ghh@kakao.com>
2023-10-18 02:27:07 +0900

    Feat: Late binding을 위한 @StepScope와 SpEL

--- src/main/kotlin/com/kyle/springbatchstudy/config/MyBatchConfig.kt
+++ src/main/kotlin/com/kyle/springbatchstudy/config/MyBatchConfig.kt
@@ -1,13 +1,17 @@
 package com.kyle.springbatchstudy.config
 
 import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobParametersValidator
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.core.job.DefaultJobParametersValidator
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.support.JdbcTransactionManager
@@ -25,16 +29,19 @@ class MyBatchConfig {
     @Bean
     fun step(jobRepository: JobRepository, transactionManager: JdbcTransactionManager): Step {
         return StepBuilder("step1", jobRepository)
-            .tasklet(helloWorldTasklet(), transactionManager)
+            .tasklet(helloWorldTasklet(null), transactionManager)
             .build()
     }
 
+    @StepScope
     @Bean
-    fun helloWorldTasklet(): Tasklet {
+    fun helloWorldTasklet(
+        @Value("#{jobParameters['name']}") name: String?
+    ): Tasklet {
 
         return Tasklet { contribution, chunkContext ->
-            val name = chunkContext.stepContext
-                .jobParameters["-name"] as String
+//            val name = chunkContext.stepContext
+//                .jobParameters["-name"] as String
 
             println("Hello, $name!")
 

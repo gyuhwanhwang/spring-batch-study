02dc70d99607f314437d822043e8bb3b7c4f443e
gyuhwanhwang <ghh@kakao.com>
2023-10-22 01:41:55 +0900

    Feat: 태스크릿 이해하기 : CallableTaskletAdapter

---
+++ src/main/kotlin/com/kyle/springbatchstudy/config/CallableTaskletConfiguration.kt
@@ -0,0 +1,55 @@
+package com.kyle.springbatchstudy.config
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.core.step.tasklet.CallableTaskletAdapter
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.support.JdbcTransactionManager
+import java.util.concurrent.Callable
+
+@Configuration
+class CallableTaskletConfiguration {
+
+    companion object : Log()
+
+    @Bean
+    fun callableJob(jobRepository: JobRepository, transactionManager: JdbcTransactionManager): Job {
+        return JobBuilder("callableJob", jobRepository)
+            .incrementer(DailyJobTimeStamper())
+            .start(callableStep(jobRepository, transactionManager))
+            .build()
+    }
+
+    @Bean
+    fun callableStep(jobRepository: JobRepository, transactionManager: JdbcTransactionManager): Step {
+        return StepBuilder("callableStep", jobRepository)
+            .tasklet(tasklet(), transactionManager)
+            .build()
+    }
+
+    @Bean
+    fun tasklet(): CallableTaskletAdapter {
+        val callableTaskletAdapter = CallableTaskletAdapter()
+
+        callableTaskletAdapter.setCallable(callableObject())
+
+        return callableTaskletAdapter
+    }
+
+    @Bean
+    fun callableObject(): Callable<RepeatStatus> {
+        return Callable {
+            log.info("This was executed in another thread.")
+            RepeatStatus.FINISHED
+        }
+    }
+
+
+}
--- src/main/kotlin/com/kyle/springbatchstudy/config/MyBatchConfig.kt
+++ src/main/kotlin/com/kyle/springbatchstudy/config/MyBatchConfig.kt
@@ -21,7 +21,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.support.JdbcTransactionManager
 
-@Configuration
+//@Configuration
 class MyBatchConfig {
 
     @Bean

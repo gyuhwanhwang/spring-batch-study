3ad326882b73c121717a71435fd3f5e32aeadf1a
gyuhwanhwang <ghh@kakao.com>
2023-10-24 01:53:12 +0900

    Feat: SystemCommandTasklet 추가 구성 방법

--- src/main/kotlin/com/kyle/springbatchstudy/config/SystemCommandJobConfiguration.kt
+++ src/main/kotlin/com/kyle/springbatchstudy/config/SystemCommandJobConfiguration.kt
@@ -5,9 +5,11 @@ import org.springframework.batch.core.Step
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.batch.core.step.tasklet.SimpleSystemProcessExitCodeMapper
 import org.springframework.batch.core.step.tasklet.SystemCommandTasklet
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.task.SimpleAsyncTaskExecutor
 import org.springframework.jdbc.support.JdbcTransactionManager
 
 @Configuration
@@ -33,11 +35,24 @@ class SystemCommandJobConfiguration {
         val systemCommandTasklet = SystemCommandTasklet()
 
 //        systemCommandTasklet.setCommand("rm -rf /tmp.txt")
-        systemCommandTasklet.setCommand("rm", "-rf", "/Users/gyuhwan/tmp.txt")
-
+//        systemCommandTasklet.setCommand("rm", "-rf", "/Users/gyuhwan/tmp.txt")
+        systemCommandTasklet.setCommand("touch", "/Users/gyuhwan/proj/book/spring batch guide/spring-batch-study/tmp.txt")
         systemCommandTasklet.setTimeout(5000) // 비동기로 실행되기 때문에
         systemCommandTasklet.setInterruptOnCancel(true)
 
+        systemCommandTasklet.setWorkingDirectory("/Users/gyuhwan/proj/book/spring batch guide/spring-batch-study")
+
+        systemCommandTasklet.setSystemProcessExitCodeMapper(touchCodeMapper())
+        systemCommandTasklet.setTerminationCheckInterval(5000)
+        systemCommandTasklet.setTaskExecutor(SimpleAsyncTaskExecutor())
+        systemCommandTasklet.setEnvironmentParams(arrayOf("JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home",
+                                                          "BATCH_HOME=/Users/gyuhwan/proj/book/spring batch guide/spring-batch-study"))
+
         return systemCommandTasklet
+    }
+
+    @Bean
+    fun touchCodeMapper(): SimpleSystemProcessExitCodeMapper {
+        return SimpleSystemProcessExitCodeMapper()
     }
 }
